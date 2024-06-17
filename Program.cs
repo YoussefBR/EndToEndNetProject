6ef3692a78608d965f0ae2af2384c68b11015107
YoussefBR <yzb5142@psu.edu>
2024-06-16 17:28:37 -0700

    Finished basic student cli tool

--- Program.cs
+++ Program.cs
@@ -1,10 +1,58 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using Microsoft.VisualBasic;
 using NameGenerator.Generators;
 
 namespace HelloWorld{
     class Program{
         static void Main(string[] args){
+            List<Student> students = new List<Student>();
+            while(true){
+                Console.WriteLine("Enter command (enter h to see list of commands):");
+                string[] command = Console.ReadLine().Split(" ");
+                switch(command[0]){
+                    case "h":
+                        if(command.Length != 1) goto InputError;
+                        Console.WriteLine("l - list students\nd <NAME> - delete students that match <NAME>\na <FULL NAME> <AGE> - add a student with the name <FULL NAME> and age <AGE>\nq - quit program");
+                        break;
+                    case "l":
+                        if(command.Length != 1) goto InputError;
+                        if(students.Count == 0) Console.WriteLine("No students");
+                        foreach(Student s in students){
+                            Console.WriteLine(s.Name + "'s age is " + s.Age);
+                        }
+                        break;
+                    case "a":
+                        if(command.Length != 3) goto InputError;
+                        Student student = new Student(command[1], int.Parse(command[2]));
+                        students.Add(student);
+                        Console.WriteLine(student.Name + " has been added");
+                        break;
+                    InputError:
+                        Console.WriteLine("Incorrect amount of args for command type");
+                        break;
+                    case "d":
+                        if(command.Length != 2) goto InputError;
+                        int count = 0;
+                        for(int i = 0; i < students.Count; i++){
+                            if(students[i].Name.Contains(command[1])){
+                                students.Remove(students[i]);
+                                i--; // account for cahnge in List size
+                                count++;
+                            }
+                        }
+                        Console.WriteLine(count + " students removed");
+                        break;
+                    case "q":
+                        return;
+                    default:
+                        Console.WriteLine("Unrecognized command, try again");
+                        break;
+                }
+            }
+        }
+
+        static void parseAges(string ages){
             Console.WriteLine("Enter some number separated by spaces:");
             string line = Console.ReadLine();
             // Method 1
@@ -31,6 +79,10 @@ namespace HelloWorld{
         public Student(int age){
             Age = age;
             Name = nameGenerator.Generate();
+        }
+        public Student(string name, int age){
+            Age = age;
+            Name = name;
         }
     }
 }
