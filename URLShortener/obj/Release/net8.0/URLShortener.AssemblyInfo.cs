2acdbeccce40f277fd5efff013c905b7840f0a3b
YoussefBR <yzb5142@psu.edu>
2024-06-29 17:50:24 -0700

    Azure webapp built and linked

--- URLShortener/obj/Release/net8.0/URLShortener.AssemblyInfo.cs
+++ URLShortener/obj/Release/net8.0/URLShortener.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("URLShortener")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+818df326817ffe9c4662434026b8654422f6e4aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("URLShortener")]
